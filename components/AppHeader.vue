dfd04d48c10168a1fde6e24a0ba833d3daa550a9
Jarrett Meyer <meyer_jarrett@lilly.com>
2023-05-05 08:33:06 -0400

    header is sticky to the top

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,9 +1,36 @@
+<script lang="ts" setup></script>
+
 <template>
-  <header id="app-header" class="navbar bg-body-secondary">
+  <header id="app-header" class="navbar bg-body-secondary sticky">
     <div class="container">
-      <a class="navbar-brand">D3.js + Vue 3</a>
+      <div class="w-100 row">
+        <div class="col">
+          <NuxtLink to="/" class="navbar-brand">D3.js + Vue 3</NuxtLink>
+        </div>
+        <div class="col text-end">
+          <a
+            href="https://github.com/jarrettmeyer/d3js-vue3-examples"
+            target="_blank"
+            ><BIGithub
+          /></a>
+        </div>
+      </div>
     </div>
   </header>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+a {
+  color: white;
+  font-size: 1.25rem;
+}
+
+#app-header {
+  z-index: 100;
+  position: sticky;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 3rem;
+}
+</style>
